10b36aa41f2abb5c0898c74464ac0bdd852e21ae
fmsthird <floranteIII.miranda.sugian@gmail.com>
2022-12-29 05:36:16 +0800

    update jobs

--- src/components/job/job-area.jsx
+++ src/components/job/job-area.jsx
@@ -2,13 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { useState } from 'react'
 
-const uniqueItems = [
-  {
-    id: 1,
-    title: 'Javascript Developer',
-    time: 'Full Time',
-  },
-]
+const uniqueItems = []
 
 const JobArea = () => {
   const [jobItems] = useState(uniqueItems)
@@ -20,38 +14,46 @@ const JobArea = () => {
           Open Positions
         </h3>
         <div className="row d-flex justify-content-center">
-          {jobItems.map(item => {
-            const { id, time, title } = item
+          {jobItems.length > 0 ? (
+            jobItems.map(item => {
+              const { id, time, title } = item
 
-            return (
-              <div key={id} className="col-xl-3 grid-item cat2">
-                <div className="jobitem mb-30 bg-white">
-                  <div className="jobitem__title">
-                    <h4 className="job-title-sm">
-                      <a>{title}</a>
-                    </h4>
-                  </div>
-                  <div className="jobitem__details">
-                    <p className="mb-0">
-                      <i className="fal fa-map-marker-alt" />
-                      Cabanatuan City, NE
-                    </p>
-                    <p className="mb-0">
-                      <i className="fal fa-clock" />
-                      {time}
-                    </p>
-                  </div>
-                  <div className="jobitem__button">
-                    <Link href="mailto:techstacks.2022@gmail.com">
-                      <button className="tp-btn-border-sm w-100">
-                        Apply now
-                      </button>
-                    </Link>
+              return (
+                <div key={id} className="col-xl-3 grid-item cat2">
+                  <div className="jobitem mb-30 bg-white">
+                    <div className="jobitem__title">
+                      <h4 className="job-title-sm">
+                        <a>{title}</a>
+                      </h4>
+                    </div>
+                    <div className="jobitem__details">
+                      <p className="mb-0">
+                        <i className="fal fa-map-marker-alt" />
+                        Cabanatuan City, NE
+                      </p>
+                      <p className="mb-0">
+                        <i className="fal fa-clock" />
+                        {time}
+                      </p>
+                    </div>
+                    <div className="jobitem__button">
+                      <Link href="mailto:techstacks.2022@gmail.com">
+                        <button className="tp-btn-border-sm w-100">
+                          Apply now
+                        </button>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })
+          ) : (
+            <img
+              src="/assets/img/no-posting.svg"
+              alt="No job vacancy"
+              className="w-50"
+            />
+          )}
         </div>
       </div>
     </div>
