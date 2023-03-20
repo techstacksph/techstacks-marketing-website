4ea605a28bbebaf48ccbf144d0d79bff8567e8a0
fmsthird <floranteIII.miranda.sugian@gmail.com>
2023-03-20 23:43:23 +0800

    apply free website audit form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
-import { Wrapper } from '../layout'
+import Banner from '../components/banner'
 import HomeMain from '../components/homes/home'
 import SEO from '../components/seo'
+import { Wrapper } from '../layout'
 
 export default function index() {
   return (
     <Wrapper>
+      <Banner />
+
       <SEO noindex={false} pageTitle="Home" />
       <HomeMain />
     </Wrapper>
