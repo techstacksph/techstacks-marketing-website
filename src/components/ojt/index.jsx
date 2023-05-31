e45f3a71818f8eb98b4c8822b786dd4d89308223
Jayvee Ramos <127916787+AraBelle-0023@users.noreply.github.com>
2023-05-31 17:58:52 +0800

    finalizing benefits page and adding cycle page

--- src/components/ojt/index.jsx
+++ src/components/ojt/index.jsx
@@ -5,6 +5,7 @@ import { animationCreate } from '../../utils/utils'
 import Breadcrumb from '../common/breadcrumb/breadcrumb'
 import OjtHeroArea from './ojt-hero'
 import ServiceAreaOjt from './ojt-benefits'
+import OjtCycleArea from './ojt-cycle'
 
 const OjtCorner = () => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const OjtCorner = () => {
       {/* <Breadcrumb title="Kickstart your journey and build your career in Web Development with Techstacks OJT Program" /> */}
       <OjtHeroArea />
       <ServiceAreaOjt />
+      <OjtCycleArea />
       <FooterTwo />
     </Wrapper>
   )
