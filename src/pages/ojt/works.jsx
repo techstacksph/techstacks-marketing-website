2d2e2f8905753b7d55ee50407656562c5c9f7403
johnairafeleo <32088082+johnairafeleo@users.noreply.github.com>
2023-06-20 15:04:01 +0800

    initial-commit

--- src/pages/ojt/works.jsx
+++ src/pages/ojt/works.jsx
@@ -1,13 +1,15 @@
 import SEO from '../../components/seo'
 import Wrapper from '../../layout/wrapper'
 import OJTPortfolio from '../../components/ojt/ojt-portfolio'
+import HeaderWithSlider from '../../components/ojt/ojt.header'
 
 const WorksPage = () => {
   return (
     <Wrapper>
       <SEO noindex={false} pageTitle="OJT Works" />
-
+      <HeaderWithSlider></HeaderWithSlider>
       <OJTPortfolio></OJTPortfolio>
+
     </Wrapper>
   )
 }
