27c45324280709078a4f29d03cf186e628641ed4
fmsthird <floranteIII.miranda.sugian@gmail.com>
2023-06-22 05:16:28 +0800

    remove banner

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,3 @@
-import Banner from '../components/banner'
 import HomeMain from '../components/homes/home'
 import SEO from '../components/seo'
 import { Wrapper } from '../layout'
@@ -6,8 +5,6 @@ import { Wrapper } from '../layout'
 export default function index() {
   return (
     <Wrapper>
-      <Banner />
-
       <SEO noindex={false} pageTitle="Home" />
 
       <HomeMain />
