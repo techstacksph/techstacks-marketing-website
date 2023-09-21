44931c65eb4269069774b8ae88c14c5b6d04f847
franzbrnrdn <franz17.b@gmail.com>
2023-09-21 09:47:58 +0800

    modified footer

---
+++ src/constants/footer-link.ts
@@ -0,0 +1,23 @@
+export const aboutLink = [
+  { title: 'Mission', url: '#' },
+  { title: 'Our team', url: '#' },
+  { title: 'Awards', url: '#' },
+  { title: 'Testimonials', url: '#' },
+  { title: 'Privacy policy', url: '#' },
+];
+
+export const servicesLink = [
+  { title: 'Web design', url: '#' },
+  { title: 'Web development', url: '#' },
+  { title: 'Mobile design', url: '#' },
+  { title: 'UI/UX design', url: '#' },
+  { title: 'Branding design', url: '#' },
+];
+
+export const portfolioLink = [
+  { title: 'Corporate websites', url: '#' },
+  { title: 'E-commerce', url: '#' },
+  { title: 'Mobile apps', url: '#' },
+  { title: 'Landing pages', url: '#' },
+  { title: 'UI/UX projects', url: '#' },
+];
