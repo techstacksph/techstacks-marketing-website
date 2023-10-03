8a542aad2d7eccf0b8d170f39fd31a6c285174c7
fmsthird <floranteIII.miranda.sugian@gmail.com>
2023-10-04 03:35:08 +0800

    dont track about us

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,23 @@
+import Head from 'next/head'
 import React from 'react'
+import AboutMain from '../components/about'
 import SEO from '../components/seo'
 import { Wrapper } from '../layout'
-import AboutMain from '../components/about'
 
 const index = () => (
-  <Wrapper>
-    <SEO noindex={false} pageTitle="About" />
-    <AboutMain />
-  </Wrapper>
+  <>
+    <Head>
+      <script
+        type="text/javascript"
+        async=""
+        src="https://www.googletagmanager.com/gtag/js?id=G-9K7DRGWDDH&l=dataLayer&cx=c"
+      ></script>
+    </Head>
+    <Wrapper>
+      <SEO noindex={false} pageTitle="About" />
+      <AboutMain />
+    </Wrapper>
+  </>
 )
 
 export default index
