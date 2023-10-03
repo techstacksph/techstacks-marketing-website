71d77c6cd89bc4b34e3c3aa6579e379ff2fdc3cc
fmsthird <floranteIII.miranda.sugian@gmail.com>
2023-10-04 04:13:43 +0800

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,23 +1,13 @@
-import Head from 'next/head'
 import React from 'react'
 import AboutMain from '../components/about'
 import SEO from '../components/seo'
 import { Wrapper } from '../layout'
 
 const index = () => (
-  <>
-    <Head>
-      <script
-        type="text/javascript"
-        async=""
-        src="https://www.googletagmanager.com/gtag/js?id=G-9K7DRGWDDH&l=dataLayer&cx=c"
-      ></script>
-    </Head>
-    <Wrapper>
-      <SEO noindex={false} pageTitle="About" />
-      <AboutMain />
-    </Wrapper>
-  </>
+  <Wrapper>
+    <SEO noindex={false} pageTitle="About" />
+    <AboutMain />
+  </Wrapper>
 )
 
 export default index
