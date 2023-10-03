a9ed4243127f51394b0d1ea244392b8230c70d8d
fmsthird <floranteIII.miranda.sugian@gmail.com>
2023-10-04 05:13:17 +0800

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
@@ -13,16 +14,18 @@ import '../styles/index.scss'
 
 const MyApp = ({ Component, pageProps }) => (
   <>
-    <script> 
-      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PTNRQ3KQ');`}
-    </script>
     <GoogleAnalytics trackPageViews />
     <Provider store={store}>
       <Messenger />
+      <Script id="gtm-tech">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PTNRQ3KQ');
+        `}
+      </Script>
       <Component {...pageProps} />
       <ToastContainer />
       <CookieConsentComponent />
