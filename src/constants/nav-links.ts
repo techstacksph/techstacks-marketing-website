8eca6653f6764c82d0b215ed2d432912a7cbb8f8
franzbrnrdn <franz17.b@gmail.com>
2023-10-03 09:53:33 +0800

    Used Link by next/link

--- src/constants/nav-links.ts
+++ src/constants/nav-links.ts
@@ -14,25 +14,25 @@ export const companyLink = [
         label: 'Our team',
         href: NavRoutes.Company.OurTeam,
         description:
-          'Incididunt ullamco esse tempor aliqua laboris minim est tempor quis adipisicing tempor anim et. Fugiat ipsum ea ipsum ex adipisicing laboris aliqua excepteur fugiat. Ea non veniam fugiat culpa culpa incididunt laborum officia mollit. Dolore amet ipsum aute incididunt. Consectetur ut ad excepteur consectetur irure dolore nisi pariatur voluptate tempor veniam. Labore esse Lorem labore do exercitation officia minim eiusmod anim ullamco in.',
+          'Get to know the people behind our success—the dynamic individuals that form our web development family.',
       },
       {
         label: 'Blogs',
         href: NavRoutes.Company.Blogs,
         description:
-          'Aliquip irure veniam eiusmod ipsum nulla est culpa do culpa. Excepteur ad occaecat cillum excepteur adipisicing irure velit reprehenderit ea id voluptate anim cillum excepteur. Magna nostrud sunt esse et excepteur aute. Cillum id dolor do irure consequat. Dolor quis fugiat consectetur qui exercitation id Lorem laboris est duis irure cupidatat labore.',
+          'Explore, learn, and embark on a journey towards digital excellence.',
       },
       {
         label: 'Portfolios',
         href: NavRoutes.Company.Portfolios,
         description:
-          'Quis cupidatat dolor voluptate incididunt magna aute nulla tempor consectetur non excepteur dolor elit. Laborum dolor laborum ad laboris magna reprehenderit in sint velit. Non id veniam reprehenderit excepteur nostrud anim magna occaecat cupidatat non et laborum dolore nulla. Anim amet irure mollit ea deserunt aute. Nostrud occaecat tempor eu fugiat cupidatat et laboris sunt consequat esse.',
+          'Dive into a diverse collection of designs, showcasing our ability to blend creativity with impactful digital solutions.',
       },
       {
         label: 'Careers',
         href: NavRoutes.Company.Careers,
         description:
-          'Officia aliquip consequat incididunt tempor consequat velit. Ullamco mollit sunt Lorem in aute voluptate ea qui eu eu amet tempor est. Laborum reprehenderit tempor est minim in in occaecat Lorem duis Lorem. Veniam dolor incididunt non quis occaecat Lorem ipsum officia officia deserunt sit est magna. Non consectetur sunt eu reprehenderit elit elit nisi est quis mollit consectetur anim officia exercitation. Ullamco consequat duis proident sint adipisicing dolor incididunt magna occaecat quis qui quis ut. Mollit ullamco Lorem ullamco culpa magna ad deserunt labore.',
+          'Discover exciting career prospects at our firm, where innovation meets expertise.',
       },
     ],
   },
