fff7911242f587d5730bac08a4cb81a4e9943257
Marcus <techstacksph.marcus@gmail.com>
2023-10-17 14:44:33 +0800

    refactor: refactored methodology section

--- src/components/website-lifecycle.tsx
+++ src/components/website-lifecycle.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { Player } from '@lottiefiles/react-lottie-player';
-import { useState } from 'react';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
+import { Fragment } from 'react';
 import { cn } from '@/utils/cn';
+import { Separator } from './ui/separator';
 
 export const METHOD_CONTENT = [
   [
@@ -33,70 +33,49 @@ export const METHOD_CONTENT = [
   ],
 ] as const;
 
-type MethodTabValue = (typeof METHOD_CONTENT)[number][number];
-
 export default function WebsiteLifecycle() {
-  const [tab, setTab] = useState<MethodTabValue>(METHOD_CONTENT[0][0]);
-  const [container] = useAutoAnimate();
-
   return (
-    <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
-      <div className="grid grid-cols-3 gap-2 md:grid-cols-1">
-        {METHOD_CONTENT.map(([title], i) => (
-          <div
-            className={cn(
-              'border rounded border-muted-foreground-static p-2 transition',
-              title === tab && 'bg-primary-static',
-            )}
-            key={title}
-            onMouseEnter={() => {
-              setTab(title);
-            }}
-          >
-            <div className="flex h-full gap-2">
+    <div className="flex flex-col items-center md:block">
+      {METHOD_CONTENT.map(([title, content, lottieFile], i) => {
+        const isOdd = Boolean(i % 2);
+
+        return (
+          <Fragment key={title}>
+            <div className="transition-all border rounded-md border-border/25 md:border-0 md:px-8">
               <div
-                className={cn(
-                  'grow text-sm text-muted truncate md:flex md:items-center md:text-lg md:font-medium',
-                  title === tab && 'text-primary-foreground-static',
-                )}
+                className="grid md:grid-cols-[1fr_1px_1fr] gap-x-8 items-center"
+                data-aos="fade-up"
               >
-                {title}
-              </div>
-              <div
-                className={cn(
-                  'self-end -mb-2 -mr-1 font-medium text-muted',
-                  title === tab && 'text-primary-foreground-static',
-                )}
-              >
-                0{i + 1}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div
-        className="border rounded border-muted-foreground-static md:col-span-2"
-        ref={container}
-      >
-        {METHOD_CONTENT.map(
-          ([title, description, lottieFile]) =>
-            title === tab && (
-              <div className="px-4 py-8" key={title}>
-                <div className="grid grid-cols-2 gap-2">
-                  <div className="flex items-center justify-center">
-                    <h3 className="text-xl">{title}</h3>
+                <div className={cn('md:py-4', isOdd && 'md:order-last')}>
+                  <div className="p-4 rounded-lg md:border md:bg-primary/50">
+                    <div className="flex items-center justify-between gap-4">
+                      <div className="space-y-2">
+                        <h3 className="text-3xl font-medium">{title}</h3>
+                        <p className="text-muted">{content}</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex items-center content-center w-full aspect-square shrink-0 md:row-span-2 [&>*]:w-full">
-                    <Player autoplay loop src={lottieFile} />
-                  </div>
-                  <div className="col-span-2 md:col-span-1">
-                    <p className="text-lg text-muted">{description}</p>
+                </div>
+
+                <Separator orientation="vertical" />
+
+                <div className={cn('py-4', isOdd && 'md:order-first')}>
+                  <div className="md:h-96 h-36 [&>*]:h-full flex justify-center">
+                    <Player autoplay className="h-full" loop src={lottieFile} />
                   </div>
                 </div>
               </div>
-            ),
-        )}
-      </div>
+            </div>
+
+            {i < METHOD_CONTENT.length - 1 ? (
+              <Separator
+                className="h-24 md:hidden bg-border/25"
+                orientation="vertical"
+              />
+            ) : null}
+          </Fragment>
+        );
+      })}
     </div>
   );
 }
