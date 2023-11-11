002b779e838e964b89357e4a516843713dd8613a
Marcus <techstacksph.marcus@gmail.com>
2023-11-12 00:06:04 +0800

    refactor: abstracted glow card

--- src/components/benefit-card.tsx
+++ src/components/benefit-card.tsx
@@ -1,8 +1,6 @@
-'use client';
-
-import { useCallback, useEffect, useRef } from 'react';
 import { Separator } from './ui/separator';
 import { H3 } from './ui/typography';
+import { GlowCard, GlowCardContent } from './glow-card';
 
 interface BenefitCardProps {
   index: number;
@@ -11,41 +9,12 @@ interface BenefitCardProps {
 }
 
 function BenefitCard({ index, title, description }: BenefitCardProps) {
-  const glowRef = useRef<HTMLDivElement>(null);
-  const hiddenRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = useCallback((evt: MouseEvent) => {
-    const rec = hiddenRef.current?.getBoundingClientRect();
-
-    glowRef.current?.animate(
-      [
-        {
-          transform: `translate(${
-            evt.clientX - (rec?.left || 0) - (rec?.width || 0) / 2
-          }px,${evt.clientY - (rec?.top || 0) - (rec?.height || 0) / 2}px)`,
-        },
-      ],
-      {
-        duration: 300,
-        fill: 'forwards',
-      },
-    );
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [handleMouseMove]);
-
   return (
-    <div
-      className="relative p-1 overflow-hidden rounded-lg"
+    <GlowCard
       data-aos={index % 2 ? 'fade-left' : 'fade-right'}
       data-aos-delay={index * 100}
     >
-      <div className="w-full h-full pt-3 pb-4 transition rounded-lg px-7 bg-background/50 backdrop-blur-3xl">
+      <GlowCardContent className="w-full h-full pt-3 pb-4 px-7">
         <div className="flex flex-col justify-between h-full gap-4">
           <div className="space-y-4">
             <H3 className="space-y-4">
@@ -59,19 +28,8 @@ function BenefitCard({ index, title, description }: BenefitCardProps) {
 
           <Separator className="h-1 bg-primary-static" />
         </div>
-      </div>
-
-      <div
-        className="absolute -z-[1] top-0 left-0 w-64 h-64 rounded-full bg-primary-static/30 blur-2xl"
-        ref={glowRef}
-        role="none"
-      />
-      <div
-        className="absolute -z-[1] top-0 left-0 w-52 h-52 rounded-full invisible"
-        ref={hiddenRef}
-        role="none"
-      />
-    </div>
+      </GlowCardContent>
+    </GlowCard>
   );
 }
 
--- src/components/core-services.tsx
+++ src/components/core-services.tsx
@@ -1,15 +1,13 @@
-'use client';
-
-import { useCallback, useEffect, useRef } from 'react';
 import type { StaticImageData } from 'next/image';
 import Image from 'next/image';
 import { Separator } from './ui/separator';
+import { GlowCard, GlowCardContent } from './glow-card';
 
 interface CoreServicesCardProps {
   index: number;
-  title?: string;
-  description?: string;
-  src?: StaticImageData;
+  title: string;
+  description: string;
+  src: StaticImageData;
 }
 
 function CoreServicesCard({
@@ -18,47 +16,19 @@ function CoreServicesCard({
   title,
   description,
 }: CoreServicesCardProps) {
-  const glowRef = useRef<HTMLDivElement>(null);
-  const hiddenRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = useCallback((evt: MouseEvent) => {
-    const rec = hiddenRef.current?.getBoundingClientRect();
-
-    glowRef.current?.animate(
-      [
-        {
-          transform: `translate(${
-            evt.clientX - (rec?.left || 0) - (rec?.width || 0) / 2
-          }px,${evt.clientY - (rec?.top || 0) - (rec?.height || 0) / 2}px)`,
-        },
-      ],
-      {
-        duration: 300,
-        fill: 'forwards',
-      },
-    );
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [handleMouseMove]);
-
   return (
-    <div
-      className="relative p-1 overflow-hidden rounded-lg h-full"
+    <GlowCard
+      className="h-full"
       data-aos="fade-up"
       data-aos-delay={index * 100}
     >
-      <div className="w-full h-full p-8 lg:p-10 transition rounded-lg  bg-background/50 backdrop-blur-3xl">
+      <GlowCardContent className="w-full h-full p-8 lg:p-10">
         <div className="flex flex-col justify-between h-full gap-4">
           <div className="space-y-4">
-            <div className="h-10 w-20">
-              {src ? <Image alt={`${title} icon`} src={src} /> : null}
+            <div className="w-20 h-10">
+              <Image alt={`${title} icon`} src={src} />
             </div>
-            <h3 className="space-y-4 text-lg md:text-2xl font-bold">
+            <h3 className="space-y-4 text-lg font-bold md:text-2xl">
               <span className="block">{title}</span>
             </h3>
             <p className="text-lg text-muted">{description}</p>
@@ -66,19 +36,8 @@ function CoreServicesCard({
 
           <Separator className="h-1 bg-primary-static" />
         </div>
-      </div>
-
-      <div
-        className="absolute -z-[1] top-0 left-0 w-64 h-64 rounded-full bg-primary-static/80 blur-2xl"
-        ref={glowRef}
-        role="none"
-      />
-      <div
-        className="absolute -z-[1] top-0 left-0 w-52 h-52 rounded-full invisible"
-        ref={hiddenRef}
-        role="none"
-      />
-    </div>
+      </GlowCardContent>
+    </GlowCard>
   );
 }
 
