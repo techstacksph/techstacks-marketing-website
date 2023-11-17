af960f0c15df1b0e878016a3fe3e0b47f59cf886
Marcus <techstacksph.marcus@gmail.com>
2023-11-17 14:45:25 +0800

    feat: added base design of portfolio page

---
+++ src/app/company/portfolios/@hero/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useWindowScroll } from '@uidotdev/usehooks';
+import { useMemo, useRef } from 'react';
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
+import { Section } from '@/components/default-elements';
+import { H1, Subheading } from '@/components/ui/typography';
+
+export default function PortfoliosHero() {
+  const [{ y }] = useWindowScroll();
+  const sectionRef = useRef<HTMLElement>(null);
+
+  const visibility = useMemo(() => {
+    const scrollProgress = y ?? 0;
+    const sectionHeight = sectionRef.current?.offsetHeight ?? 0;
+    const FULL_PERCENT = 100 as const;
+    const SECTION_HALF = sectionHeight / 2;
+
+    const value =
+      (FULL_PERCENT - (scrollProgress / SECTION_HALF) * FULL_PERCENT) /
+      FULL_PERCENT;
+
+    return value > 0 ? value : 0;
+  }, [y]);
+
+  return (
+    <Section
+      className="min-h-[calc(theme(height.screen)-theme(height.16))] sticky top-[theme(height.16)]"
+      ref={sectionRef}
+      style={{ opacity: visibility, scale: visibility }}
+    >
+      <div className="flex flex-col gap-8 items-center min-h-[inherit] justify-center">
+        <H1 className="text-center">
+          Showcasing our Digital{' '}
+          <span className="text-primary-static">Excellence</span>
+        </H1>
+        <Subheading className="text-center">
+          Explore our digital portfolio, a fusion of cutting-edge innovation and
+          tangible impact, reflecting our commitment to pushing boundaries and
+          delivering results.
+        </Subheading>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <Link
+            className="relative inline-flex items-center justify-start w-full p-3 overflow-hidden font-medium transition-all bg-transparent border rounded md:w-52 h-14 border-primary-static group"
+            // TODO: where to route
+            href="#"
+          >
+            <span className="w-48 h-48 rounded rotate-[-40deg] bg-primary-static absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0" />
+            <span className="relative w-full text-center text-black transition-colors duration-300 ease-in-out dark:text-white group-hover:text-black">
+              Get started <ArrowRight className="inline" />
+            </span>
+          </Link>
+          <Link
+            className="relative inline-flex items-center justify-start w-full p-3 overflow-hidden font-medium transition-all bg-transparent border rounded md:w-52 h-14 border-primary-static group"
+            // TODO: where to route
+            href="#"
+          >
+            <span className="w-48 h-48 rounded rotate-[-40deg] bg-primary-static absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0" />
+            <span className="relative w-full text-center text-black transition-colors duration-300 ease-in-out dark:text-white group-hover:text-black">
+              Learn more
+            </span>
+          </Link>
+        </div>
+      </div>
+    </Section>
+  );
+}
--- src/components/latest-works-carousel.tsx
+++ src/components/latest-works-carousel.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import { TRAINING_PLANS } from '@/constants/training-plans';
 import { cn } from '@/utils/cn';
+import { projectItems } from '@/constants/project-items';
 import { Carousel, CarouselItem } from './ui/carousel';
-import { H3, Subheading } from './ui/typography';
+import { H3, H4, Subheading } from './ui/typography';
 
-export function TrainingsCarousel() {
+export function LatestWorksCarousel() {
   return (
     <Carousel
       className={cn(
@@ -12,19 +12,19 @@ export function TrainingsCarousel() {
         // Override overflow class of slider without using `!important`
         '[&&&]:overflow-x-visible [&&&]:overflow-y-clip',
         // Fade effects
-        'before:bg-gradient-to-b before:from-background before:from-5% before:to-transparent before:to-25% before:absolute before:pointer-events-none before:top-0 before:left-0 before:w-full before:h-full before:z-10',
-        'after:bg-gradient-to-t after:from-background after:from-5% after:to-transparent after:to-25% after:absolute after:pointer-events-none after:top-0 after:left-0 after:w-full after:h-full after:z-10',
+        'before:bg-gradient-to-b before:from-background before:from-5% before:to-transparent before:to-25% before:absolute before:pointer-events-none before:top-0 before:left-0 before:w-full before:h-full before:z-[1]',
+        'after:bg-gradient-to-t after:from-background after:from-5% after:to-transparent after:to-25% after:absolute after:pointer-events-none after:top-0 after:left-0 after:w-full after:h-full after:z-[1]',
       )}
       opts={{
         slides: {
-          origin: 'center',
           perView: 1.8,
+          origin: 'center',
         },
         vertical: true,
         mode: 'free-snap',
       }}
     >
-      {TRAINING_PLANS.map(([title, description, img], idx) => (
+      {projectItems.map(({ category, img, subtitle, title }, idx) => (
         <CarouselItem
           className={cn(
             'py-4 group data-[ready=false]:opacity-0 transition-opacity',
@@ -36,19 +36,20 @@ export function TrainingsCarousel() {
         >
           <div className="h-full group-data-[active=false]:scale-95 transition-transform rounded-2xl border-primary-static border shadow-lg shadow-primary-static">
             <div className="h-full p-6 xl:p-8">
-              <div className="flex flex-col items-center justify-center h-full gap-4 md:justify-start md:flex-row bg-background">
-                <div className="hidden w-36 h-36 md:block shrink-0">
+              <div className="flex flex-col items-center justify-center h-full gap-4 md:justify-start md:flex-row">
+                <div className="hidden w-auto h-full aspect-square md:block shrink-0">
                   <Image
                     alt={title}
                     className="object-contain w-full h-full"
-                    height={50}
                     src={img}
-                    width={50}
                   />
                 </div>
                 <div className="flex flex-col gap-4">
+                  <H4 asChild>
+                    <Subheading>{category}</Subheading>
+                  </H4>
                   <H3>{title}</H3>
-                  <Subheading>{description}</Subheading>
+                  <Subheading>{subtitle}</Subheading>
                 </div>
               </div>
             </div>
