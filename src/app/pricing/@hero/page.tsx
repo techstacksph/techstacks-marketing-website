89c5b1019cf0d08472e9955e385fa3b31d05169a
Marcus <techstacksph.marcus@gmail.com>
2023-11-20 16:33:44 +0800

    refactor: refactored pricing content to fix build errors

--- src/app/pricing/@hero/page.tsx
+++ src/app/pricing/@hero/page.tsx
@@ -5,14 +5,14 @@ import {
   PricingProvider,
   PricingSwitch,
 } from '@/components/pricing-card';
-import { H2 } from '@/components/ui/typography';
+import { H1, Subheading } from '@/components/ui/typography';
 
 export default function PricingHero() {
   return (
     <PricingProvider>
       <Section className="flex flex-col items-center gap-20">
         <div className="flex flex-col items-center gap-4">
-          <H2 className="[&&]:leading-normal text-center whitespace-pre-wrap  font-dm-sans">
+          <H1 className="text-center whitespace-pre-wrap">
             Your{' '}
             <span className="font-black text-primary-foreground">
               Perfect Plan
@@ -22,15 +22,15 @@ export default function PricingHero() {
             <span className="font-black text-primary-foreground">
               Perfect Business
             </span>
-          </H2>
-          <p className="text-lg font-medium text-center text-foreground/75">
+          </H1>
+          <Subheading className="text-center">
             Tailored Solutions to Fuel Your Success
-          </p>
+          </Subheading>
         </div>
 
         <PricingSwitch />
 
-        <div className="flex flex-col gap-4 overflow-visible md:overflow-hidden lg:overflow-visible md:gap-0 md:flex-row lg:gap-11 md:shadow-lg lg:shadow-none [&&]:shadow-primary-static/30 md:rounded-lg lg:rounded-none">
+        <div className="flex flex-col gap-4 overflow-visible lg:overflow-visible md:gap-0 md:flex-row lg:gap-11 md:shadow-lg lg:shadow-none [&&]:shadow-primary-static/30 md:rounded-lg lg:rounded-none">
           {PRICING.map((pricing) => (
             <PricingCard {...pricing} key={pricing.name} />
           ))}
