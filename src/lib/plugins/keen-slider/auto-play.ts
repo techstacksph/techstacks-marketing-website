827e924c113d5dd352ce358a749a660f2e3e6355
Marcus <34080311+marcus-dc@users.noreply.github.com>
2023-11-28 15:02:01 +0800

    style: component (#78)
    * style: modified styles of component
    
    * refactor: added compatible progress bar compoenent

--- src/lib/plugins/keen-slider/auto-play.ts
+++ src/lib/plugins/keen-slider/auto-play.ts
@@ -1,30 +1,32 @@
 import { type KeenSliderPlugin } from 'keen-slider/react';
 
-export const AutoPlay: KeenSliderPlugin = (slider) => {
-  let timeout: ReturnType<typeof setTimeout>;
-  let mouseOver = false;
-  function clearNextTimeout() {
-    clearTimeout(timeout);
-  }
-  function nextTimeout() {
-    clearTimeout(timeout);
-    if (mouseOver) return;
-    timeout = setTimeout(() => {
-      slider.next();
-    }, 2000);
-  }
-  slider.on('created', () => {
-    slider.container.addEventListener('mouseover', () => {
-      mouseOver = true;
-      clearNextTimeout();
-    });
-    slider.container.addEventListener('mouseout', () => {
-      mouseOver = false;
+export function AutoPlay(time: number): KeenSliderPlugin {
+  return (slider) => {
+    let timeout: ReturnType<typeof setTimeout>;
+    let mouseOver = false;
+    function clearNextTimeout() {
+      clearTimeout(timeout);
+    }
+    function nextTimeout() {
+      clearTimeout(timeout);
+      if (mouseOver) return;
+      timeout = setTimeout(() => {
+        slider.next();
+      }, time);
+    }
+    slider.on('created', () => {
+      slider.container.addEventListener('mouseover', () => {
+        mouseOver = true;
+        clearNextTimeout();
+      });
+      slider.container.addEventListener('mouseout', () => {
+        mouseOver = false;
+        nextTimeout();
+      });
       nextTimeout();
     });
-    nextTimeout();
-  });
-  slider.on('dragStarted', clearNextTimeout);
-  slider.on('animationEnded', nextTimeout);
-  slider.on('updated', nextTimeout);
-};
+    slider.on('dragStarted', clearNextTimeout);
+    slider.on('animationEnded', nextTimeout);
+    slider.on('updated', nextTimeout);
+  };
+}
