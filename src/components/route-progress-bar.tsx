9d2f331f037c378e8067a8bfb2f5936d9b5dc89e
Marcus <techstacksph.marcus@gmail.com>
2023-11-28 14:37:17 +0800

    refactor: added compatible progress bar compoenent

--- src/components/route-progress-bar.tsx
+++ src/components/route-progress-bar.tsx
@@ -2,18 +2,19 @@
 
 import { useTheme } from 'next-themes';
 import dynamic from 'next/dynamic';
-import NextTopLoader from 'nextjs-toploader';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 function Loader() {
   const { resolvedTheme } = useTheme();
+
   return (
-    <NextTopLoader
+    <ProgressBar
       color={
         resolvedTheme === 'dark'
           ? 'hsl(var(--primary-static))'
           : 'hsl(var(--accent))'
       }
-      showSpinner={false}
+      options={{ showSpinner: false }}
     />
   );
 }
