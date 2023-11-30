0e80e30b49891dbca849b2af3477e7febe9d597c
franzbrnrdn <franz17.b@gmail.com>
2023-11-30 10:12:39 +0800

    refactor website lifecycle methodology

--- src/components/website-lifecycle.tsx
+++ src/components/website-lifecycle.tsx
@@ -1,100 +1,93 @@
 'use client';
 
-import { Player } from '@lottiefiles/react-lottie-player';
-import { Fragment } from 'react';
 import { cn } from '@/utils/cn';
-import { isOdd as utilIsOdd } from '@/utils/is-odd';
-import { Separator } from './ui/separator';
+import { isOdd } from '@/utils/is-odd';
 import { Section } from './default-elements';
 import { H2, H3, Subheading } from './ui/typography';
+import { SvgDesignProcess } from './icons/svg-design-process-icon';
+import { SvgPlanning } from './icons/svg-planning-icon';
+import { SvgDevelopment } from './icons/svg-development-icon';
+import { SvgOptimize } from './icons/svg-optimize-icon';
+import { SvgSupport } from './icons/svg-support-icon';
 
 export const METHOD_CONTENT = [
   [
     'Planning & Ideation',
     'Discussion about scope of work, deliverables and project cost',
-    '/lottie/planning.json',
+    SvgPlanning,
   ],
   [
     'Design Process',
     'Design system, blueprints, wireframes, prototypes, and hi-fidelity designs',
-    '/lottie/design.json',
+    SvgDesignProcess,
   ],
   [
     'Development',
     'Incremental builds, integrations, code reviews, and quality assurance',
-    '/lottie/code.json',
+    SvgDevelopment,
   ],
   [
     'Optimize & Scale',
     'Evaluate post-deploy performance and SEO score, optimize, and scale.',
-    '/lottie/optimize.json',
+    SvgOptimize,
   ],
   [
     'Support',
     'Experience unparalleled assistance with our extensive technical / dev support.',
-    '/lottie/support.json',
+    SvgSupport,
   ],
 ] as const;
 
 export default function WebsiteLifecycle() {
   return (
-    <div className="flex justify-center w-full py-16">
+    <div className="w-full flex justify-center py-16">
       <Section>
-        <div className="flex flex-col items-center gap-10">
-          <div className="flex flex-col items-center gap-8">
-            <Subheading className="text-center" data-aos="fade-up">
-              Our Methodology
-            </Subheading>
-            <H2 className="text-center" data-aos="fade-up" data-aos-delay={100}>
-              Systematic flow in the website lifecycle
+        <div className="flex flex-col gap-10 lg:gap-32">
+          <div
+            className="flex flex-col gap-2"
+            data-aos="fade-up"
+            data-aos-delay={100}
+          >
+            <Subheading className="text-center">Our Methodology</Subheading>
+            <H2 className="text-center">
+              Systematic flow in the{' '}
+              <span className="text-primary-static">website lifecycle</span>
             </H2>
           </div>
-          <div className="flex flex-col items-center md:block">
-            {METHOD_CONTENT.map(([title, content, lottieFile], i) => {
-              const isOdd = utilIsOdd(i);
+          <div className="flex flex-col items-center gap-2 md:gap-0">
+            {METHOD_CONTENT.map(([title, content, Icon], index) => {
+              const odd = isOdd(index);
 
               return (
-                <Fragment key={title}>
-                  <div className="transition-all border rounded-md border-border/25 md:border-0 md:px-8">
-                    <div
-                      className="grid md:grid-cols-[1fr_1px_1fr] gap-x-8 items-center"
-                      data-aos="fade-up"
+                <div
+                  className={cn(
+                    'flex flex-row items-center gap-2 sm:gap-4 md:gap-8 m-0 lg:mr-[500px] lg:ml-0 lg:-mt-[65px]',
+                    odd && 'flex-row-reverse m-0 lg:mr-0 lg:ml-[500px]',
+                  )}
+                  data-aos="fade-up"
+                  data-aos-delay={index + 100}
+                  key={title}
+                >
+                  <div className="w-full max-w-sm flex flex-col gap-4 p-4 border border-primary-static bg-primary-static/10">
+                    <H3 className={cn('text-left', odd && 'text-right')}>
+                      {title}
+                    </H3>
+                    <Subheading
+                      className={cn(
+                        'text-left leading-none md:leading-loose',
+                        odd && 'text-right',
+                      )}
                     >
-                      <div className={cn('md:py-4', isOdd && 'md:order-last')}>
-                        <div className="p-4 rounded-lg md:border md:bg-primary/50">
-                          <div className="flex items-center justify-between gap-4">
-                            <div className="space-y-2">
-                              <H3>{title}</H3>
-                              <Subheading className="text-muted">
-                                {content}
-                              </Subheading>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-
-                      <Separator orientation="vertical" />
-
-                      <div className={cn('py-4', isOdd && 'md:order-first')}>
-                        <div className="md:h-96 h-36 [&>*]:h-full flex justify-center">
-                          <Player
-                            autoplay
-                            className="h-full"
-                            loop
-                            src={lottieFile}
-                          />
-                        </div>
-                      </div>
-                    </div>
+                      {content}
+                    </Subheading>
                   </div>
-
-                  {i < METHOD_CONTENT.length - 1 ? (
-                    <Separator
-                      className="h-24 md:hidden bg-border/25"
-                      orientation="vertical"
-                    />
-                  ) : null}
-                </Fragment>
+                  <Icon
+                    className={cn(
+                      'w-40 h-40',
+                      odd && 'w-40 h-40 xl:w-80 xl:h-80',
+                    )}
+                  />
+                </div>
               );
             })}
           </div>
