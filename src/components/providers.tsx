73f6dc6ffd4b50196a3b8b451cf42ac68f341a00
Marcus <techstacksph.marcus@gmail.com>
2023-12-05 11:40:13 +0800

    added google analytics

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { useEffect, type PropsWithChildren } from 'react';
 import AOS from 'aos';
+import { env } from '@/env.mjs';
 
 export function Providers({ children }: PropsWithChildren) {
   useEffect(() => {
@@ -10,14 +12,20 @@ export function Providers({ children }: PropsWithChildren) {
   }, []);
 
   return (
-    <ThemeProvider
-      defaultTheme="system"
-      enableColorScheme
-      enableSystem
-      storageKey="theme"
-      themes={['light', 'dark', 'system']}
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics
+        gaMeasurementId={env.NEXT_PUBLIC_GA_MEASUREMENT_ID}
+        trackPageViews
+      />
+      <ThemeProvider
+        defaultTheme="system"
+        enableColorScheme
+        enableSystem
+        storageKey="theme"
+        themes={['light', 'dark', 'system']}
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
--- src/env.mjs
+++ src/env.mjs
@@ -7,9 +7,12 @@ export const env = createEnv({
   server: {
     WORDPRESS_API_URL: z.string(),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string(),
+  },
   runtimeEnv: {
     WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
+    NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
   },
   skipValidation: SKIP_VALIDATION_SCRIPTS.some(
     (script) => process.env.npm_lifecycle_event === script,
