6d9047ec804ce619499553eb92c97d738b5a6313
Marcus <34080311+marcus-dc@users.noreply.github.com>
2023-12-11 14:08:31 +0800

    fix: smooth scroll (#98)
    * fix: smooth scrolling
    
    * fix: SEO metadata

--- src/utils/create-metadata.ts
+++ src/utils/create-metadata.ts
@@ -16,5 +16,9 @@ export function createMetadata({
     description: description
       ? description
       : 'Techstacks - team of developers and designers committed to help our clients stand out and take advantage of the web by creating beautiful, user-friendly, fast, secure, high converting, engaging websites. Building websites with a purpose. Hire us today and accelerate your business growth.',
+    metadataBase:
+      process.env.NODE_ENV !== 'development'
+        ? new URL('https://techstacksph.com/')
+        : null,
   };
 }
