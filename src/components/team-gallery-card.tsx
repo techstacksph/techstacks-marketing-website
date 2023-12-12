d6e88dda31076d3039d9a1e4e2d27f51d77694bc
franzbrnrdn <franz17.b@gmail.com>
2023-12-12 17:09:51 +0800

    enhancement our team testimonials

--- src/components/team-gallery-card.tsx
+++ src/components/team-gallery-card.tsx
@@ -1,4 +1,6 @@
 import Image, { type StaticImageData } from 'next/image';
+import { BsQuote } from 'react-icons/bs';
+import { cn } from '@/utils/cn';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { H3, Subheading } from './ui/typography';
 
@@ -8,6 +10,7 @@ interface TeamGalleryCardProps {
   position: string;
   title: string;
   body: string;
+  index: boolean;
 }
 
 export function TeamGalleryCard({
@@ -16,9 +19,15 @@ export function TeamGalleryCard({
   position,
   title,
   body,
+  index,
 }: TeamGalleryCardProps) {
   return (
-    <div className="border border-border rounded-2xl">
+    <div
+      className={cn(
+        'border border-primary-static rounded-2xl shadow-lg [&&]:shadow-primary-static/40',
+        index && 'bg-primary-static/80',
+      )}
+    >
       <div className="p-8 space-y-4">
         <div className="flex items-center gap-4">
           <Avatar className="w-12 h-12">
@@ -29,14 +38,21 @@ export function TeamGalleryCard({
           </Avatar>
           <div>
             <H3>{name}</H3>
-            <Subheading>{position}</Subheading>
+            <Subheading className="text-foreground">{position}</Subheading>
           </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-4">
           <H3 asChild>
             <p>{title}</p>
           </H3>
-          <Subheading>{body}</Subheading>
+          <Subheading className="text-foreground">
+            <span className="relative inline-block">
+              <span className="absolute top-0 left-0 -z-10 text-8xl dark:text-muted-foreground/50 text-muted-foreground/30">
+                <BsQuote />
+              </span>
+              {body}
+            </span>
+          </Subheading>
         </div>
       </div>
     </div>
