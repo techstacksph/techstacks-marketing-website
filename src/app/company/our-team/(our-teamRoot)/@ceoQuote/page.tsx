2e467a2660cc8ad2a0f2cbb58606c98975bbbb5b
Third Sugian <floranteIII.miranda.sugian@gmail.com>
2024-04-18 18:45:33 +0800

    Revert "remove third"
    This reverts commit 007ecef46a87cf6b06f26b29e190f448969e56d8.

--- src/app/company/our-team/(our-teamRoot)/@ceoQuote/page.tsx
+++ src/app/company/our-team/(our-teamRoot)/@ceoQuote/page.tsx
@@ -25,7 +25,9 @@ export default function CeoQuote() {
                 Let&apos;s talk about your ideas and how Techstacks can help
                 bring it to life.
               </Subheading>
-              <Subheading className="font-bold italic text-center lg:text-md mt-8" />
+              <Subheading className="font-bold italic text-center lg:text-md mt-8">
+                fms third
+              </Subheading>
               <div className="text-3xl xl:text-5xl self-end">
                 <BiSolidQuoteAltRight />
               </div>
