1842d907312a185e707b263a58f394fe44fb77b4
fmsthird <floranteiii.miranda.sugian@gmail.com>
2024-11-06 13:35:10 +0800

    undo document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import {
+  type DocumentProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+function MyDocument({ __NEXT_DATA__ }: DocumentProps) {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        {/* <noscript>
+          <iframe
+            height="0"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PTNRQ3KQ"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="gtag-frame"
+            width="0"
+          />
+        </noscript> */}
+      </body>
+    </Html>
+  );
+}
+
+export default MyDocument;
