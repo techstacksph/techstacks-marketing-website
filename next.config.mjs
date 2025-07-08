2ec18ab436aa1920c414d1f2d5c81f8a5666e459
Third Sugian <floranteIII.miranda.sugian@gmail.com>
2025-07-08 19:02:07 +0800

    fix-build

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
         hostname: 'www.datocms-assets.com',
-        pathname: '/**',
       },
     ],
   },
