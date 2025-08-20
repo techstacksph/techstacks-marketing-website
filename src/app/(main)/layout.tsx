a2190ad4bf6ed3ee13dee34f07d1c162ac6b49e5
Third Sugian <floranteIII.miranda.sugian@gmail.com>
2025-08-21 01:06:30 +0800

    update

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,23 +2,14 @@
 import { type ReactNode } from 'react';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import MessengerChat from '@/components/fb-messenger';
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
       {children}
-      <script dangerouslySetInnerHTML={{ __html: `window.fbAsyncInit = function() {
-          FB.init({
-            appId            : '1337528334395112',
-            xfbml            : true,
-            version          : 'v23.0'
-          });
-        };` }} />
-      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
-      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment -- no */}
-      {/* @ts-expect-error */}
-      <div className="fb-messengermessageus" color="blue" messenger_app_id="1337528334395112" page_id="2385228971773232" size="standard" />
+      <MessengerChat />
       <Footer />
     </>
   );
---
+++ src/components/fb-messenger.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable -- no */
+"use client"
+import { useEffect } from "react";
+
+export default function MessengerChat() {
+  useEffect(() => {
+    if (document.getElementById("facebook-jssdk")) return;
+
+    const script = document.createElement("script");
+    script.id = "facebook-jssdk";
+    script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    (window as any).fbAsyncInit = () => {
+      (window as any).FB.init({
+        xfbml: true,
+        version: "v23.0",
+      });
+    };
+  }, []);
+
+  return (
+      <div
+        id="fb-customer-chat"
+        // @ts-ignore
+        attribution="biz_inbox"
+        className="fb-customerchat"
+        page_id="103524484853667"
+      />
+  );
+}
+ 
