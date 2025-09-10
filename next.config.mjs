a67095eba2b817d4abec40c548aecb10f9b6046f
Third Sugian <floranteIII.miranda.sugian@gmail.com>
2025-09-10 12:41:40 +0800

    disable types

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,13 @@ await import('./src/env.mjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
