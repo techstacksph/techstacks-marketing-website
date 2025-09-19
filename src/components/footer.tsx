809a1d32781b949c1216656adc70736b53d6a7b5
Third Sugian <floranteIII.miranda.sugian@gmail.com>
2025-09-19 11:36:03 +0800

    update link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -90,7 +90,7 @@ export default function Footer() {
                         className="flex flex-col justify-center font-semibold text-primary-static"
                         key={page.title}
                       >
-                        <Link href={page.href}>{page.title}</Link>
+                        <Link href={page.href as string}>{page.title}</Link>
                       </li>
                     ))}
                   </ul>
